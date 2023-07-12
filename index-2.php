f20ec6f4a63c9c66688fc641951f4aea316f012d
ESTHEROWN <108894712+ESTHEROWN@users.noreply.github.com>
2023-07-12 16:31:37 +0000

    conx in the page index

--- index-2.php
+++ index-2.php
@@ -1,6 +1,23 @@
+
+<?php
+$hostname = base64_encode("ABJ-LAP-297\OPERASLOCAL");
+$port = 1433;
+$dbname = base64_encode("GESTIONSCHOOL");
+$username = base64_encode("sa");
+$pw = base64_encode("Operas@db22"); 
+try {
+  $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+  $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES French_100_CS_AI';
+  $bdd = new PDO("sqlsrv:Server=" . base64_decode($hostname) . ";Database=" . base64_decode($dbname) . "", base64_decode("$username"), base64_decode("$pw"), $pdo_options);
+} catch (PDOException $e) {
+  print "Erreur!Pas de connexion à la base de données <br/>". $e->getMessage() ;
+   var_dump($e); die();
+  
+  //header("Location:$errorPage", true, 307);
+}
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
